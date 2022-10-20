332badef380eeab9dbdb8a5bc7f827bffd253658
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-10-20 13:19:49 +0300

    add: Logout button

--- ltuc_portal/lib/Screens/home_screen.dart
+++ ltuc_portal/lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +12,16 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(actions: [
+        TextButton(
+          onPressed: () => FirebaseAuth.instance.signOut(),
+          child: Icon(
+            Icons.logout_outlined,
+            color: Color.fromARGB(237, 129, 5, 5),
+            size: 36.0,
+          ),
+        )
+      ]),
       body: Container(child: Text("HOME ")),
     );
   }
