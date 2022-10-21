e47c7ca96a5b8ca7aaa86e9a1e2434e86c1e5344
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-21 19:39:35 +0300

    Create students_mark.dart

---
+++ ltuc_portal/lib/utility/students_mark.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Marks extends DataTableSource {
+  final List<Map<String, dynamic>> _data = List.generate(
+      200,
+      (index) => {
+            "id": index,
+            "title": "Item $index",
+            "score": Random().nextInt(101)
+          });
+
+  @override
+  DataRow? getRow(int index) {
+    return DataRow(cells: [
+      DataCell(Text(_data[index]['id'].toString())),
+      DataCell(Text(_data[index]["title"])),
+      DataCell(Text(_data[index]["score"].toString())),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => _data.length;
+
+  @override
+  int get selectedRowCount => 0;
+}
