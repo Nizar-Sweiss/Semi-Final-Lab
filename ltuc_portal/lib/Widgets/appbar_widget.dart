aa0236533e2cd638013ef9fd4b0fff4848a58896
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-22 21:47:08 +0300

    fix img_error

--- ltuc_portal/lib/Widgets/appbar_widget.dart
+++ ltuc_portal/lib/Widgets/appbar_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ltuc_portal/utility/utility.dart';
 
 // ignore: non_constant_identifier_names
@@ -13,7 +12,7 @@ AppBarWidget(context) {
         Row(
           children: [
             Image.asset(
-              "images/LTUC_logo_black.png",
+              "assets/images/LTUC_logo_black.png",
               height: 20,
             ),
           ],
